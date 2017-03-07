fa90f19d51feca0393824ade152d2beb6765fa69
Diego van Haaster <diegomvh@gmail.com>
2017-03-07 13:01:42 -0300

    Full schema

--- src/NetAuthServer/Models/User.cs
+++ src/NetAuthServer/Models/User.cs
@@ -2,7 +2,7 @@
 {
     public class MongoDbUser
     {
-        public string Id { get; set; }
+        public ObjectId Id { get; set; }
         public string Username { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
