07d860d913ab55f3267623a8944e6a6c3b7058e1
Diego van Haaster <diegomvh@gmail.com>
2017-03-08 12:23:18 -0300

    Sln and global.json

--- test/Mongo.Tests/UnitTest1.cs
+++ test/Mongo.Tests/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System;
 using Xunit;
 
 namespace Mongo.Tests
@@ -6,9 +5,14 @@ namespace Mongo.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void PassingTest()
         {
+            Assert.Equal(4, Add(2, 2));
+        }
 
+        int Add(int x, int y)
+        {
+            return x + y;
         }
     }
 }
